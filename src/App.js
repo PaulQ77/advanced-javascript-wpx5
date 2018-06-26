5a12a11ae41c6ce9dfb3e82adc8483282a2f31b0
Paul Quiroz <pquiroz1977@gmail.com>
2018-06-26 11:13:21 -0700

    new commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CommentsList from ''
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CommentsList/>
+      </div>
+    );
+  }
+}
+
+export default App;
